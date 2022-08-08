64b3472134f3545cdea183d49208519e40444e20
griand <gaastech@gmail.com>
2022-08-09 00:55:10 +0300

    Update index.js
    logging

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const app = require('express')();
 const proxy = require('express-http-proxy');
 const port = process.env.PORT || 8080;
+const audit = require('express-requests-logger')
+const bunyan = require('bunyan');
+const logger = bunyan.createLogger({name: 'myapp'});
 
 const selectHost = (req) => {
   return `https://${req.params.region}-${req.params.projectId}.cloudfunctions.net`;
 };
+
+app.use(audit({logger: logger}}));
 
 app.use(
   '/functions/:region/:projectId',
